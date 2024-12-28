0f3002d67a4052433063488172f1a5924dac9d92
amirul akmal <amirul.akmal.putrajaya2003@gmail.com>
2024-12-28 12:28:41 +0800

    New Tools [Alertifyjs & Livewire]

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,11 @@
 import './bootstrap';
-
+import alertify from 'alertifyjs';
+import.meta.glob([
+    '../images/**',
+]);
 import Alpine from 'alpinejs';
+
+window.alertify = alertify;
 
 window.Alpine = Alpine;
 
